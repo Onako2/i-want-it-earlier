fa29e9f47914eb68a84422fb6144ab47380fe19e
稀神灵梦 <touhouer@vip.qq.com>
2024-10-10 20:23:20 +0800

    upload

--- src/main/java/rs/onako2/iwie/entity/CreakingEntity.java
+++ src/main/java/rs/onako2/iwie/entity/CreakingEntity.java
@@ -8,13 +8,26 @@ import net.minecraft.entity.ai.pathing.PathNodeType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.ZombieVillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.world.World;
 import rs.onako2.iwie.Util;
 import rs.onako2.iwie.entity.ai.goal.CreakingMeleeAttackGoal;
 
 public class CreakingEntity extends HostileEntity {
+    private static final TrackedData<Boolean> IS_HEART_SPAWN = DataTracker.registerData(CreakingEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+
+    public boolean isHeartSpawn = false;
+    public boolean deathLock = false;
+
     public CreakingEntity(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
         this.setPathfindingPenalty(PathNodeType.WATER, -1.0F);
@@ -29,6 +42,37 @@ public class CreakingEntity extends HostileEntity {
     }
 
     @Override
+    public void tick() {
+        super.tick();
+        if(this.getWorld().isClient()) {
+            return;
+        }
+        if(this.isHeartSpawn && this.getWorld().isDay() || (this.isHeartSpawn && !Util.isHeartNear(this, this.getWorld(), 16))) {
+            this.deathLock = true;
+            this.kill();
+        }
+    }
+    @Override
+    protected void initDataTracker(DataTracker.Builder builder) {
+        super.initDataTracker(builder);
+        builder.add(IS_HEART_SPAWN,false);
+    }
+
+    @Override
+    public void writeCustomDataToNbt(NbtCompound nbt) {
+        super.writeCustomDataToNbt(nbt);
+        nbt.putBoolean("is_heart_spawn", this.isHeartSpawn);
+    }
+
+    @Override
+    public void readCustomDataFromNbt(NbtCompound nbt) {
+        super.readCustomDataFromNbt(nbt);
+        if (nbt.contains("is_heart_spawn", NbtElement.BYTE_TYPE)) {
+            this.isHeartSpawn = nbt.getBoolean("is_heart_spawn");
+        }
+    }
+
+    @Override
     protected void initGoals() {
         this.goalSelector.add(1, new CreakingMeleeAttackGoal(this, 1.0, true));
         this.goalSelector.add(2, new WanderAroundGoal(this, 0.7));
@@ -38,10 +82,28 @@ public class CreakingEntity extends HostileEntity {
 
     @Override
     public boolean damage(DamageSource source, float amount) {
+        if(this.isHeartSpawn && !this.deathLock) {
+            return false;
+        }
 
         if (!Util.isHeartNear(this, this.getWorld(), 16)) {
             return super.damage(source, amount);
         }
+
         return false;
+    }
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return SoundEvents.ENTITY_SKELETON_AMBIENT;
+    }
+
+    @Override
+    protected SoundEvent getHurtSound(DamageSource source) {
+        return SoundEvents.ENTITY_SKELETON_HURT;
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.ENTITY_SKELETON_DEATH;
     }
 }
